c3e14bf58cbc7040816fec2c0f0ded645dce497c
Quentin Dstl <qdesautel@gmail.com>
2021-10-18 14:01:53 +0200

    test images de profile

--- tp4/src/App.js
+++ tp4/src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 import data from './Users.json'
 
+var user_nb = 0;
+
+function changeUser(id) {
+  console.log("test");
+  user_nb = id;
+}
+
+
 class User extends React.Component {
   render() {
     return (
-      <button className="User">
+      <button className="User" onClick={changeUser(this.value)}>
         {data[this.props.value].name}
       </button>
     )
@@ -16,21 +24,25 @@ class Content extends React.Component {
   render() {
     return (
       <section className="Content">
-        test
+        <img src={data[this.props.value].picture} alt="pp"></img>
+        {data[this.props.value].picture}
       </section>
     )
   }
 }
 
-function App() {
+function App() { 
   return (
     <main className="App">
+      <div></div>
+      <div></div>
       <header className="App-header">
         <User value={0}/>
         <User value={1}/>
         <User value={2}/>
       </header>
-      <Content />
+      <div></div>
+      <Content value={user_nb}/>
     </main>
   );
 }
