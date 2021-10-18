652f28c266467e534900589bc4528abf3f635a5f
Quentin Dstl <qdesautel@gmail.com>
2021-10-18 13:38:28 +0200

    button nom + json data

--- tp4/src/App.js
+++ tp4/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import data from './Users.json'
+
+class User extends React.Component {
+  render() {
+    return (
+      <button className="User">
+        {data[this.props.value].name}
+      </button>
+    )
+  }
+}
+
+class Content extends React.Component {
+  render() {
+    return (
+      <section className="Content">
+        test
+      </section>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <User value={0}/>
+        <User value={1}/>
+        <User value={2}/>
       </header>
-    </div>
+      <Content />
+    </main>
   );
 }
 
